2de725170a9a44e4b37a222782c0d478127fc81c
AbdullahShokr1 <darkthe254@gmail.com>
2025-12-02 13:13:58 +0200

    properties APIs

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,16 +5,13 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
-import { provideHttpClient } from '@angular/common/http';
 import Aura from '@primeuix/themes/aura';
-
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-
-
+import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MessageService } from 'primeng/api'; // ← مهم جداً
+import { MessageService } from 'primeng/api';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -28,7 +25,7 @@ export const appConfig: ApplicationConfig = {
         preset: Aura,
       },
     }),
-    provideHttpClient(),
     provideAnimations(),
+    MessageService
   ],
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,14 +8,22 @@ import { StudentProfile } from './features/student-profile/student-profile';
 import { OwnerProfile } from './features/owner-profile/owner-profile';
 import { FilterPage } from './features/filter-page/filter-page';
 import { Search } from './features/search/search';
+import { ErrorPage } from './features/error-page/error-page';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { Unauthorized } from './features/unauthorized/unauthorized';
+import { NoAuthGuard } from './core/guards/noAuth.guard';
+import { UserRoleGuard } from './core/guards/userRole.guard';
 import { PropertyDetail } from './features/property-detail/property-detail';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'home', component: Home },
   { path: 'aboutus', component: AboutUs },
   { path: 'contactus', component: Contactus },
   { path: 'filter', component: FilterPage },
   { path: 'search', component: Search },
+
   {
     path: 'owner-dashboard',
     component: OwnerDashboard,
@@ -26,9 +34,7 @@ export const routes: Routes = [
     canActivate: [UserRoleGuard],
     data: { role: 'owner' }
   },
-
-
-
+  { path: 'properties/:id', component: PropertyDetail },
 
 
 // Prevent logged users from accessing login/register
@@ -43,13 +49,20 @@ export const routes: Routes = [
     canActivate: [NoAuthGuard]
   },
 
+  // Student-only page
   {
     path: 'profile-student',
     component: StudentProfile,
+    canActivate: [UserRoleGuard],
+    data: { role: 'student' }
   },
+
+
+  // Unauthorized page
   {
-    path: 'profile-owner',
-    component: OwnerProfile,
+    path: 'unauthorized',
+    component: Unauthorized
   },
-  { path: 'properties/:id', component: PropertyDetail },
+  // MUST ALWAYS BE LAST
+  { path: '**', component: ErrorPage },
 ];
