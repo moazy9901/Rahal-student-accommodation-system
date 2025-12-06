47c09a8862e45c05ad688f036cdf57c3f95743a1
saadsafwat4444 <saadsafwat2522@gmail.com>
2025-12-06 05:43:58 +0200

    add fav to students

--- backend/app/Http/Controllers/Api/V1/PropertySaveController.php
+++ backend/app/Http/Controllers/Api/V1/PropertySaveController.php
@@ -35,9 +35,91 @@ class PropertySaveController extends Controller
         ]);
     }
 
+    // public function myFavourites()
+    // {
+    //     $user = Auth::user();
+
+    //     $properties = $user->savedProperties()
+    //         ->with('images') // ترجع الصور أيضاً
+    //         ->get();
+
+    //     return response()->json($properties);
+    // }
+
+    // public function myFavourites()
+    // {
+    //     $user = Auth::user();
+    //     return response()->json($user->savedProperties);
+    // }
+
+    // public function myFavourites()
+    // {
+    //     $user = Auth::user();
+    //     $default = asset('images/default-avatar.png');
+    //     // جلب الخصائص المحفوظة مع الصور
+    //     $properties = $user->savedProperties()
+    //         ->with('images') // ترجع كل الصور
+    //         ->get()
+    //         ->map(function ($property) {
+    //             // تعديل مسار الصور ليكون رابط كامل
+    //             $property->images = $property->images->map(function ($image) {
+    //                 $image->path = asset('storage/' . $image->path);
+    //                 return $image;
+    //             });
+
+    //             return $property;
+    //         });
+
+    //     return response()->json($properties);
+    // }
+
+    // public function myFavourites()
+    // {
+    //     $user = Auth::user();
+
+
+    //     // جلب الخصائص المحفوظة مع الصور
+    //     $properties = $user->savedProperties()
+    //         ->with('images') // ترجع كل الصور
+    //         ->get()
+    //         ->map(function ($property) use ($default) {
+
+    //             if ($property->images->isEmpty()) {
+    //                 // لو مفيش صور، خلي الصورة الافتراضية
+    //                 $property->images = collect([
+    //                     (object) ['path' => $default]
+    //                 ]);
+    //             } else {
+    //                 // تعديل مسار الصور ليكون رابط كامل
+    //                 $property->images = $property->images->map(function ($image) {
+    //                     $image->path = asset('storage/' . $image->path);
+    //                     return $image;
+    //                 });
+    //             }
+
+    //             return $property;
+    //         });
+
+    //     return response()->json($properties);
+    // }
+
     public function myFavourites()
     {
         $user = Auth::user();
-        return response()->json($user->savedProperties);
+
+        $properties = $user->savedProperties()
+            ->with('images')
+            ->get()
+            ->map(function ($property) {
+
+                $property->images = $property->images->map(function ($image) {
+                    $image->path = asset('storage/' . $image->path);
+                    return $image;
+                });
+
+                return $property;
+            });
+
+        return response()->json($properties);
     }
 }
